c9e7fbdb6959c4eabddf435d7862e15a92562d94
Samuel Banya <sbanya@fastmail.com>
2023-06-10 15:11:47 -0500

    Adding background gradient, button styling, bold headers, and switches.

--- client/App.js
+++ client/App.js
@@ -1,57 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { StyleSheet, View, useWindowDimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button, Text, Switch } from '@rneui/themed';
+import { React, useState } from "react";
 
 export default function App() {
   // Grab 'height', 'width', 'scale', and 'fontScale' using the 'useWindowDimensions' API
   // so that we can scale the app's window size accordingly:
   const { height, width, scale, fontScale } = useWindowDimensions();
 
+  const [checked, setChecked] = useState(false);
+
+  const toggleSwitch = () => {
+    setChecked(!checked);
+  };
+
   return (
-    // Overall container for app:
-    <View style={styles.appContainer}>
-      {/* Top menu for desktop web use only: */}
-      <View>
-      </View>
-      {/* Top header section: */}
-      <View>
-        <Text style={styles.header}>Registrees</Text>
-        <Button 
-          title="Add New Registree"
-        />
-      </View>
-      {/* Bottom half section for remaining content: */}
-      <View>
-        <View style={styles.registreeContainer}>
+    <LinearGradient colors={["#343534", "#1f704e", "#0ea666"]} style={styles.greenBackgroundGradient}>
+      {/* Overall container for app: */}
+      <View style={styles.appContainer}>
+        {/* Top menu for desktop web use only: */}
+        <View>
         </View>
-        <View style={styles.registreeContainer}>
+        {/* Top header section: */}
+        <View>
+          <Text style={styles.header}>Registrees</Text>
+          <Button 
+            title="Add New Registree"
+            style={styles.newRegistreeButton}
+            type="outline"
+          />
         </View>
-        <View style={styles.registreeContainer}>
+        {/* Bottom half section for remaining content: */}
+        <View>
+          <View style={styles.registreeContainer}>
+            <Text style={styles.boldHeader}>Mom's Christmas List</Text>
+            <Text>Registree.xyz/mom101</Text>
+            <Switch 
+              value={checked}
+              onValueChange={(value) => setChecked(value)}
+            />
+          </View>
+          <View style={styles.registreeContainer}>
+            <Text style={styles.boldHeader}>Sam's Birthday List</Text>
+            <Text>Registree.xyz/sam123</Text>
+            <Switch 
+              value={checked}
+              onValueChange={(value) => setChecked(value)}
+            />
+          </View>
+          <View style={styles.registreeContainer}>
+            <Text style={styles.boldHeader}>Tina's Baby Shower Registry</Text>
+            <Text>Registree.xyz/tina456</Text>
+            <Switch 
+              value={checked}
+              onValueChange={(value) => setChecked(value)}
+            />
+          </View>
+        </View>
+        {/* Buttons footer for mobile and iPad: */}
+        <View style={styles.bottomBackground}>
+          <Button 
+            title="Registrees"
+          />
+          <Button 
+            title="Share"
+          />
+          <Button 
+            title="Profile"
+          />
         </View>
       </View>
-      {/* Buttons footer for mobile and iPad: */}
-      <View>
-        <Button 
-          title="Registrees"
-        />
-        <Button 
-          title="Share"
-        />
-        <Button 
-          title="Profile"
-        />
-      </View>
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    
-    backgroundColor: 'rgb(52,53,52); linear-gradient(90deg, rgba(52,53,52,1) 0%, rgba(31,112,78,1) 50%, rgba(14,166,102,1) 100%);'
+  },
+  newRegistreeButton: {
+    backgroundColor: "#FFFFFF",
+    borderColor: "#3AFF7D",
+    borderRadius: "5px"
+  },
+  darkGreenGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
+  bottomBackground: {
+    // backgroundColor: "#10c36e"
+    color: "#000000"
   },
   header: {
     fontSize: '50px',
     color: '#FFC700'
+  },
+  boldHeader: {
+    fontWeight: 'bold'
   },
   registreeContainer: {
     height: '90px',
