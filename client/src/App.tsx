e8bd5e6e615447064f7ab5eb3330db036869b0e4
Samuel Banya <sbanya@fastmail.com>
2023-07-01 16:10:32 -0500

    Placed main files in root directory, and planned to separate others into components folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// Adding Material UI components:
+import Button from "@mui/material/Button";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>Test</h1>
+      <Button variant="contained">Click Me</Button>
+          <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>xs=6</Item>
+        </Grid>
+        <Grid item xs>
+          <Item>xs</Item>
+        </Grid>
+      </Grid>
+    </Box>
     </div>
   );
 }
