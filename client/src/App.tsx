96096657ba745573c8553cfe2a8be47f8e0f3725
Samuel Banya <sbanya@fastmail.com>
2023-07-01 15:30:21 -0500

    Adding MaterialUI dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +15,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Test</h1>
     </div>
   );
 }
