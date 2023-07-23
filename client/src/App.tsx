167f819f79a94714e851287b40951d2a2171b3ea
Samuel Banya <sbanya@fastmail.com>
2023-07-22 23:03:39 -0500

    Adding latest changes to make homepage look closer to Figma template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,12 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Switch from '@mui/material/Switch';
+import { IconButton } from '@mui/material';
+import { MdDragIndicator } from "react-icons/md";
+import { FaAmazon, FaEbay, FaClipboard } from "react-icons/fa";
+import { FaShareNodes, FaUser } from "react-icons/fa6";
+import { SiTarget } from "react-icons/si";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,21 +24,104 @@ const Item = styled(Paper)(({ theme }) => ({
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
-      <Button variant="contained">Click Me</Button>
-          <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Item>xs</Item>
+      <h1>Registrees</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Item>
+              <Button variant="contained">Add New Registrees</Button>
+            </Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+              <Grid item xs={2}>
+                <Item>
+                  <IconButton aria-label="Drag a Registree to a different position on screen">
+                    <MdDragIndicator />
+                  </IconButton>
+                </Item>
+              </Grid>
+              <Grid item xs={8}>
+                  <h2>Mom's Christmas List</h2>
+                  <h3>Registree.xyz/mom101</h3>
+                  <FaAmazon />
+              </Grid>
+              <Grid item xs={2}>
+                <Item>
+                  <Switch />
+                </Item>
+              </Grid>
+            </Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+              <Grid item xs={2}>
+                <Item>
+                  <IconButton aria-label="Drag a Registree to a different position on screen">
+                    <MdDragIndicator />
+                  </IconButton>
+                </Item>
+              </Grid>
+              <Grid item xs={8}>
+                  <h2>Sam's Birthday List</h2>
+                  <h3>Registree.xyz/sam123</h3>
+                  <FaEbay />
+              </Grid>
+              <Grid item xs={2}>
+                <Item>
+                  <Switch />
+                </Item>
+              </Grid>
+            </Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+              <Grid item xs={2}>
+                <Item>
+                  <IconButton aria-label="Drag a Registree to a different position on screen">
+                    <MdDragIndicator />
+                  </IconButton>
+                </Item>
+              </Grid>
+              <Grid item xs={8}>
+                  <h2>Tina's Wedding Registry List</h2>
+                  <h3>Registree.xyz/tina456</h3>
+                  <SiTarget />
+              </Grid>
+              <Grid item xs={2}>
+                <Item>
+                  <Switch />
+                </Item>
+              </Grid>
+            </Item>
+          </Grid>
+          <Grid item xs={12}>
+            <Item>
+                <Grid item xs={4}>
+                  <Item>
+                    <IconButton aria-label="View current user's Registrees">
+                      <FaClipboard />
+                    </IconButton>
+                  </Item>
+                </Grid>
+                <Grid item xs={4}>
+                  <Item>
+                    <IconButton aria-label="View current user's Registrees">
+                      <FaShareNodes />
+                    </IconButton>
+                  </Item>
+                </Grid>
+                <Grid item xs={4}>
+                  <Item>
+                    <IconButton aria-label="View current user's Registrees">
+                      <FaUser />
+                    </IconButton>
+                  </Item>
+                </Grid>
+            </Item>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs>
-          <Item>xs</Item>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
     </div>
   );
 }
